cc0e001caf98909a1f967cca73cf8b4b26d3df67
Julia Yusupova <cashleva.iulia@gmail.com>
2020-08-04 17:42:08 +0300

    Added EmphaSoft

---
+++ EmphaSoft/precache-manifest.df8c763ecfe7732c89c81ce381202f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4981d26d908f16164527c52040f3e19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c9525bccdba6aa4f7d",
+    "url": "/static/css/main.cd47641d.chunk.css"
+  },
+  {
+    "revision": "7545faa362aaf3fcd8b5",
+    "url": "/static/js/2.0457fef7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0457fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c9525bccdba6aa4f7d",
+    "url": "/static/js/main.a2d73c12.chunk.js"
+  },
+  {
+    "revision": "eb184fec74bfbf7c9dde",
+    "url": "/static/js/runtime-main.55837eae.js"
+  }
+]);
