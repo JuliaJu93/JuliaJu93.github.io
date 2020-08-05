69e358b19ec196f76bbc0e19456aa38136dddf35
Julia Yusupova <cashleva.iulia@gmail.com>
2020-08-05 23:55:10 +0300

    tet-task to root

---
+++ precache-manifest.145b521a035b3216de0a6eedc30bf0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aace4116fa4889cf93b8f3323239eb68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39616f670858f6ad5085",
+    "url": "./static/css/main.4a0be803.chunk.css"
+  },
+  {
+    "revision": "7545faa362aaf3fcd8b5",
+    "url": "./static/js/2.0457fef7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0457fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39616f670858f6ad5085",
+    "url": "./static/js/main.e3344b4a.chunk.js"
+  },
+  {
+    "revision": "aac3fd05799597e12def",
+    "url": "./static/js/runtime-main.a03e27d8.js"
+  }
+]);
